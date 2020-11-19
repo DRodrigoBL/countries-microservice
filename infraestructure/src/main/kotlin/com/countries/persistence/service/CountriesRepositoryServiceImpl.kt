f0ee7e1c9f70e11db2c417d8f4af5c98bb02d765
rodrigo-bautista <rodrigo.bautista@rappi.com>
2020-11-19 10:58:38 -0600

    repository service move refactor

--- infraestructure/src/main/kotlin/com/countries/persistence/service/CountriesRepositoryServiceImpl.kt
+++ infraestructure/src/main/kotlin/com/countries/persistence/service/CountriesRepositoryServiceImpl.kt
@@ -1,13 +1,9 @@
-package com.countries.persistence.repository.service
+package com.countries.persistence.service
 
 import com.countries.entities.Country
-import com.countries.persistence.entities.ContinentEnum
-import com.countries.persistence.entities.CountryTable
 import com.countries.persistence.entities.toCountryModel
 import com.countries.persistence.repository.CountriesRepository
 import com.countries.ports.CountriesRepositoryService
-import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.queryForObject
 import org.springframework.stereotype.Service
 
 @Service
