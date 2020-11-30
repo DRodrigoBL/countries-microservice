8010a68714e70aba9a88556972b2fc565430cba8
rodrigo-bautista <rodrigo.bautista@rappi.com>
2020-11-30 15:46:35 -0600

    code reformatted

--- core/src/main/kotlin/com/rappi/restaurants/core/countries/ports/gateway/web/ContinentsGatewayService.kt
+++ core/src/main/kotlin/com/rappi/restaurants/core/countries/ports/gateway/web/ContinentsGatewayService.kt
@@ -3,7 +3,5 @@ package com.rappi.restaurants.core.countries.ports.gateway.web
 import com.rappi.restaurants.core.countries.entities.Continent
 
 interface ContinentsGatewayService {
-
     fun findAvailableContinents(): List<Continent>
-
 }
--- infraestructure/src/main/kotlin/com/rappi/restaruants/infrastructure/gateway/web/continents/client/ContinentsApi.kt
+++ infraestructure/src/main/kotlin/com/rappi/restaruants/infrastructure/gateway/web/continents/client/ContinentsApi.kt
@@ -5,8 +5,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface ContinentsApi {
-
     @GET("/api/v1/continents")
     fun getAvailableContinents(): Call<ContinentsApiResponse>
-
 }
